e4197da955cbeb0fbca72a43edc59feb0ecadda9
makroma <markoma@iki.fi>
2015-02-15 20:02:40 +0200

    seed update 4 + links to navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,28 +10,28 @@ brew1 = Brewery.create name:"Koff", year:1897
 brew2 = Brewery.create name:"Malmgard", year:2001
 brew3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1 = Beer.create name:"Iso 3"
-b2 = Beer.create name:"Karhu"
-b3 = Beer.create name:"Huvila Pale Ale"
-b4 = Beer.create name:"Hefezeizen"
-b5 = Beer.create name:"Helles"
-b6 = Beer.create name:"X Porter"
+#b1 = Beer.create name:"Iso 3"
+#b2 = Beer.create name:"Karhu"
+#b3 = Beer.create name:"Huvila Pale Ale"
+#b4 = Beer.create name:"Hefezeizen"
+#b5 = Beer.create name:"Helles"
+#b6 = Beer.create name:"X Porter"
 
-brew1.beers << b1
-brew1.beers << b2
-brew1.beers << b3
-brew1.beers << b4
-brew1.beers << b5
-brew1.beers << b6
+#brew1.beers << b1
+#brew1.beers << b2
+#brew1.beers << b3
+#brew1.beers << b4
+#brew1.beers << b5
+#brew1.beers << b6
 
 s1 = Style.create name:"Lager"
 s2 = Style.create name:"Pale ale"
 s3 = Style.create name:"Porter"
 s4 = Style.create name:"Weizen"
 
-s1.beers << b1
-s1.beers << b2
-s2.beers << b3
-s2.beers << b4
+#s1.beers << b1
+#s1.beers << b2
+#s2.beers << b3
+#s2.beers << b4
 
 
