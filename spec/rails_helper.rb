923078cd835fa2fb0ae6373b470e161237de6013
makroma <markoma@iki.fi>
2015-02-15 22:59:02 +0200

    travis test fix 5

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,14 +54,18 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+
+
 module OwnTestHelper
 
   def sign_in(credentials)
     visit signin_path
-    fill_in('username', with:credentials[:username])
-    fill_in('password', with:credentials[:password])
+    fill_in "username", :with => credentials[:username]
+    fill_in "password", :with => credentials[:password]
     click_button('Log in')
   end
+
 
   def create_beer_with_rating(score, user)
     beer = FactoryGirl.create(:beer)
