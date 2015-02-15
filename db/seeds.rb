47d874507efeb74882f2d28afaa9612ade3f5210
makroma <markoma@iki.fi>
2015-02-15 19:36:40 +0200

    seeds update2

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ brew1 = Brewery.create name:"Koff", year:1897
 brew2 = Brewery.create name:"Malmgard", year:2001
 brew3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1 = Beers.create name:"Iso 3"
-b2 = Beers.create name:"Karhu"
-b3 = Beers.create name:"Huvila Pale Ale"
-b4 = Beers.create name:"Hefezeizen"
-b5 = Beers.create name:"Helles"
-b6 = Beers.create name:"X Porter"
+b1 = Beer.create name:"Iso 3"
+b2 = Beer.create name:"Karhu"
+b3 = Beer.create name:"Huvila Pale Ale"
+b4 = Beer.create name:"Hefezeizen"
+b5 = Beer.create name:"Helles"
+b6 = Beer.create name:"X Porter"
 
 
 s1 = Style.create name:"Lager"
